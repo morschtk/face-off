efc3c62440a81015fc1f6bc34ba21451c7f3eeac
Tyler Morsch <tmorsch@osx-tmorsch-mbp.lan>
2019-06-23 19:34:29 -0400

    added page for identifing which actors are in the trailer and how often. Needs that touch tho

---
+++ public/video-matcher.js
@@ -0,0 +1,70 @@
+let faceMatcher = null;
+
+async function createFaceMatcher() {
+  let theHomies = [];
+  await $.get('/whereMyPeopleAt', (data) => {
+    theHomies = data.theHomies;
+  });
+
+  // loadLabeledImages
+  const labeledFaceDescriptors = await Promise.all(
+    theHomies.map(async homie => {
+      const { label, imagesForTraining } = homie;
+      const descriptors = [];
+      for (let i=0; i<imagesForTraining.length; i++) {
+        const img = await faceapi.fetchImage(`${label}/${imagesForTraining[i]}`);
+        const detections = await faceapi.detectSingleFace(img)
+                                  .withFaceLandmarks()
+                                  .withFaceDescriptor();
+        // Should we resize these detections to the image size?
+        descriptors.push(detections.descriptor);
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptors);
+    })
+  );
+
+  return new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+}
+
+async function startThatShit() {
+  // load face detection, face landmark model and face recognition models
+  await faceapi.loadFaceRecognitionModel('/');
+  await faceapi.loadFaceLandmarkModel('/');
+  await faceapi.loadSsdMobilenetv1Model('/');
+
+  faceMatcher = await createFaceMatcher();
+  $('#loader').hide();
+  $('#content').show();
+  onPlay($('#inputVideo').get(0));
+}
+
+async function onPlay(videoEl) {
+  if(!videoEl.currentTime || videoEl.paused || videoEl.ended)
+        return setTimeout(() => onPlay(videoEl))
+
+  const canvas = $('#canvas').get(0);
+
+  faceapi.matchDimensions(canvas, videoEl, true);
+
+  const detections = await faceapi.detectAllFaces(videoEl)
+                            .withFaceLandmarks()
+                            .withFaceDescriptors();
+
+  console.log(detections);
+
+  // resize detection and landmarks in case displayed videoEl is smaller than original size
+  const resizedDetections = faceapi.resizeResults(detections, videoEl);
+
+  resizedDetections.forEach(({ detection, descriptor }) => {
+    // So what were doing here is passing the description for each face to the faceMatcher, which will return the label of the closest description it has in its bank.
+    const label = faceMatcher.findBestMatch(descriptor).toString();
+    const options = { label };
+    const drawBox = new faceapi.draw.DrawBox(detection.box, options);
+    drawBox.draw(canvas);
+  });
+  setTimeout(() => onPlay(videoEl));
+}
+
+$(document).ready(() => {
+  startThatShit();
+})
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const views_dir = path.join(__dirname, 'views');
 const img_dir = path.join(__dirname, 'images');
 app.use(express.static(views_dir));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'videos')));
 app.use(express.static(path.join(__dirname, 'face-api')));
 app.use(express.static(img_dir));
 app.use(express.static(path.join(__dirname, 'models')));
@@ -19,6 +20,7 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.get('/', (req, res) => res.redirect('/whoAreYou'));
 app.get('/whoAreYou', (req, res) => res.sendFile(path.join(views_dir, 'who-are-you.html')));
+app.get('/whosTheStar', (req, res) => res.sendFile(path.join(views_dir, 'whos-the-star.html')));
 app.get('/whereMyPeopleAt', (req, res) => {
     let labels = fs.readdirSync(img_dir);
     let theHomies = labels.map((label) => {
