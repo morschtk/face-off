0b1103181e462b2cdcc8121571fc9c1cbc4cd4ac
Tyler Morsch <tmorsch@OSX-TMORSCH-MBP.local>
2019-08-07 02:58:15 -0400

    Added models for identifing age and gender on detection screen. And added emojis

--- public/face-detection.js
+++ public/face-detection.js
@@ -1,10 +1,51 @@
 const video = document.getElementById('video');
+let predictedAges = []
+let predictedGenders = []
+const EXPRESSION_WEIGHT = 10;
+let withBoxes = true
+const EXPRESSION_MAP = {
+  'angry': '😡',
+  'disgusted': '🤢',
+  'fearful': '😰',
+  'happy': '😊',
+  'neutral': '😐',
+  'sad': '😔',
+  'surprised': '🙀'
+};
+
+function onChangeHideBoundingBoxes(e) {
+  withBoxes = !$(e.target).prop('checked')
+}
+
+function interpolateAgePredictions(age) {
+  predictedAges = [age].concat(predictedAges).slice(0, 30)
+  const avgPredictedAge = predictedAges.reduce((total, a) => total + a) / predictedAges.length
+  return avgPredictedAge
+}
+
+function interpolateGenderPredictions(gender) {
+  predictedGenders = [gender].concat(predictedGenders).slice(0, 30)
+  const avgPredictedAge = predictedGenders.reduce((total, a) => total + a) / predictedGenders.length
+  return avgPredictedAge
+}
+
+function extractExpressions(expressions) {
+  const finalExpressions = [];
+  Object.keys(expressions).forEach(key => {
+    const percent = expressions[key] * 100;
+    if (percent > EXPRESSION_WEIGHT) {
+      finalExpressions.push(`${EXPRESSION_MAP[key]} ${key.toUpperCase()}: ${faceapi.round(percent, 2)}% ${EXPRESSION_MAP[key]}`)
+    }
+  });
+  return finalExpressions;
+}
 
 async function startVideo() {
     await faceapi.loadTinyFaceDetectorModel('/');
     await faceapi.loadFaceLandmarkModel('/');
     await faceapi.loadFaceRecognitionModel('/');
     await faceapi.loadFaceExpressionModel('/');
+    await faceapi.nets.ageGenderNet.load('/');
     navigator.getUserMedia(
         { video: {} },
         stream => video.srcObject = stream,
@@ -13,7 +54,7 @@ async function startVideo() {
     $('#loader').hide();
     $('#content').css({
         'display': 'flex',
-        'justify-content': 'center',
+        'justify-content': 'space-around',
     });
 }
 
@@ -30,16 +71,47 @@ video.addEventListener('play', () => {
     setInterval(async () => {
         // What is TinyFaceDetectorOptions
         // Here we're only geting landmarks not descriptors so we can pass directions right in?
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        const detections = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
                                     .withFaceLandmarks()
+                                    .withAgeAndGender()
                                     .withFaceExpressions();
-        const resizedDetections = faceapi.resizeResults(detections, video);
+      if (detections) {
+        const resizedResult = faceapi.resizeResults(detections, video);
+
         canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height);
         // The faceMatcher is what finds the closest match not the actual api.
         // In this case the we dont need to loop through each dection to find best match.
-        faceapi.draw.drawDetections(canvas, resizedDetections);
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        if (withBoxes) {
+          faceapi.draw.drawDetections(canvas, resizedResult);
+        }
+        faceapi.draw.drawFaceLandmarks(canvas, resizedResult);
+        // faceapi.draw.drawFaceExpressions(canvas, resizedResult);
+
+        const { age, gender, genderProbability, expressions } = resizedResult;
+        // interpolate gender predictions over last 30 frames
+        // to make the displayed age more stable
+        const interpolatedAge = interpolateAgePredictions(age);
+        const interpolatedGender = interpolateGenderPredictions(genderProbability);
+        const expressionArr = extractExpressions(expressions);
+        // new faceapi.draw.DrawTextField(
+        //     [
+        //     `${faceapi.round(interpolatedAge, 0)} years`,
+        //     `${gender} (${faceapi.round(interpolatedGender)})`
+        //     ],
+        //     resizedResult.detection.box.bottomRight
+        // ).draw(canvas);
+
+        $('.gender').text(`${gender}: ${faceapi.round((interpolatedGender * 100), 0)}%`);
+        $('.age').text(`${faceapi.round(interpolatedAge, 0)} years`);
+        $('.expressions').empty();
+        expressionArr.forEach(express => {
+          $('.expressions').append(
+              $("<li>").append(
+                $("<span>").text(express)
+              )
+            );
+        });
+      }
     }, 100)
 });
 
